406918474050b0327fda025c283d2809442688b4
Arty <belovaa@mer.ci.nsu.ru>
2023-06-02 15:10:54 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Lab_12_2_Test_Application_Belov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblQuestionText = new System.Windows.Forms.Label();
+            this.btnAnswer1 = new System.Windows.Forms.Button();
+            this.btnAnswer2 = new System.Windows.Forms.Button();
+            this.btnAnswer4 = new System.Windows.Forms.Button();
+            this.btnAnswer3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 13);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(596, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // lblQuestionText
+            // 
+            this.lblQuestionText.AutoSize = true;
+            this.lblQuestionText.Location = new System.Drawing.Point(12, 54);
+            this.lblQuestionText.Name = "lblQuestionText";
+            this.lblQuestionText.Size = new System.Drawing.Size(35, 13);
+            this.lblQuestionText.TabIndex = 1;
+            this.lblQuestionText.Text = "label1";
+            // 
+            // btnAnswer1
+            // 
+            this.btnAnswer1.Location = new System.Drawing.Point(12, 151);
+            this.btnAnswer1.Name = "btnAnswer1";
+            this.btnAnswer1.Size = new System.Drawing.Size(597, 23);
+            this.btnAnswer1.TabIndex = 2;
+            this.btnAnswer1.Text = "button1";
+            this.btnAnswer1.UseVisualStyleBackColor = true;
+            this.btnAnswer1.Click += new System.EventHandler(this.btnAnswer1_Click);
+            // 
+            // btnAnswer2
+            // 
+            this.btnAnswer2.Location = new System.Drawing.Point(12, 180);
+            this.btnAnswer2.Name = "btnAnswer2";
+            this.btnAnswer2.Size = new System.Drawing.Size(597, 23);
+            this.btnAnswer2.TabIndex = 3;
+            this.btnAnswer2.Text = "button2";
+            this.btnAnswer2.UseVisualStyleBackColor = true;
+            this.btnAnswer2.Click += new System.EventHandler(this.btnAnswer2_Click);
+            // 
+            // btnAnswer4
+            // 
+            this.btnAnswer4.Location = new System.Drawing.Point(12, 238);
+            this.btnAnswer4.Name = "btnAnswer4";
+            this.btnAnswer4.Size = new System.Drawing.Size(597, 23);
+            this.btnAnswer4.TabIndex = 4;
+            this.btnAnswer4.Text = "button4";
+            this.btnAnswer4.UseVisualStyleBackColor = true;
+            this.btnAnswer4.Click += new System.EventHandler(this.btnAnswer4_Click);
+            // 
+            // btnAnswer3
+            // 
+            this.btnAnswer3.Location = new System.Drawing.Point(12, 209);
+            this.btnAnswer3.Name = "btnAnswer3";
+            this.btnAnswer3.Size = new System.Drawing.Size(597, 23);
+            this.btnAnswer3.TabIndex = 5;
+            this.btnAnswer3.Text = "button3";
+            this.btnAnswer3.UseVisualStyleBackColor = true;
+            this.btnAnswer3.Click += new System.EventHandler(this.btnAnswer3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 346);
+            this.Controls.Add(this.btnAnswer3);
+            this.Controls.Add(this.btnAnswer4);
+            this.Controls.Add(this.btnAnswer2);
+            this.Controls.Add(this.btnAnswer1);
+            this.Controls.Add(this.lblQuestionText);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblQuestionText;
+        private System.Windows.Forms.Button btnAnswer1;
+        private System.Windows.Forms.Button btnAnswer2;
+        private System.Windows.Forms.Button btnAnswer4;
+        private System.Windows.Forms.Button btnAnswer3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using static System.Random;
+
+namespace Lab_12_2_Test_Application_Belov
+{
+    public partial class Form1 : Form
+    {
+        private int currentQuestion = 1;
+        private int totalQuestions = 0;
+        private int correctAnswers = 0;
+        private string correctAnswer;
+
+        List<string> answerVariants;
+        StreamReader sr = new StreamReader("testtext.txt");
+        Random rand = new Random();
+        Button[] buttons;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttons = new Button[]{ btnAnswer1, btnAnswer2, btnAnswer3, btnAnswer4 };
+
+            using (var reader = File.OpenText(@"testtext.txt"))
+            {
+                while (reader.ReadLine() != null) { totalQuestions++; }
+            }
+            totalQuestions /= 5;
+
+            // Initialize the progress bar
+            progressBar.Minimum = 0;
+            progressBar.Maximum = totalQuestions;
+            progressBar.Value = 0;
+
+            // Display the first question
+            ShowQuestion(currentQuestion);
+        }
+
+        private void ShowQuestion(int questionNumber)
+        {
+            // Format (5 lines per question)
+            // 1. Question
+            // 2. Right answer
+            // 3. Wrong answer
+            // 4. Wrong answer
+            // 5. Wrong answer
+            lblQuestionText.Text = sr.ReadLine();
+            correctAnswer = sr.ReadLine();
+            answerVariants = new List<string>();
+            answerVariants.Add(correctAnswer);
+            for (int i = 0; i < 3; i++) answerVariants.Add(sr.ReadLine());
+
+            // Display the question and answer variants
+            // Ideally randomize for fairer tests
+            int indexTemp;
+            for (int i = 3; i > -1; i--)
+            {
+                indexTemp = rand.Next(0, i);
+                buttons[i].Text = answerVariants[indexTemp];
+                answerVariants.Remove(answerVariants[indexTemp]);
+            }
+        }
+
+        private void CheckAnswer(Button answerButton)
+        {
+            // Increase the progress bar value and the correct answer counter
+            progressBar.Value++;
+            if (answerButton.Text == correctAnswer) correctAnswers++;
+
+            // End test or show next question
+            if (currentQuestion != totalQuestions)
+            {
+                currentQuestion++;
+                ShowQuestion(currentQuestion);
+            }
+            else
+            {
+                MessageBox.Show($"Вы ответили на {correctAnswers} вопросов из {totalQuestions}");
+                Close();
+            }
+        }
+
+        // Click handlers for the answer buttons
+        private void btnAnswer1_Click(object sender, EventArgs e) { CheckAnswer(btnAnswer1); }
+        private void btnAnswer2_Click(object sender, EventArgs e) { CheckAnswer(btnAnswer2); }
+        private void btnAnswer3_Click(object sender, EventArgs e) { CheckAnswer(btnAnswer3); }
+        private void btnAnswer4_Click(object sender, EventArgs e) { CheckAnswer(btnAnswer4); }
+        }
+    }
